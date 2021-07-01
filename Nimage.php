54bb22b717665d23a89b0cf2cabe233d8e26a888
jacquelintom <jacquelintom2015@gmail.com>
2021-07-01 21:03:15 +0530

    nubel systems and batteries

---
+++ Nimage.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+    h1 {
+            padding: 40px;
+            text-shadow: 2px 2px 5px hotpink;
+            font-size: 45px;
+            font-family: Copperplate, Papyrus, fantasy;
+        }
+    img{
+            border: 5px solid white;
+            margin: 30px;
+        }
+    form{
+             background-color: #ffe6e6;
+            
+        }
+    footer  {
+                    padding: 30px;
+                     background-color:black;
+                     color: white;
+                     font-style: italic;
+
+            }
+    i       {
+                     color:white;
+            }
+</style>
+</head>
+<body>
+<form>
+    <center><h1><b>NUBEL SYSTEMS AND BATTERIES</b></h1></center>
+    <center><img src="nubel6.png" alt="nubel" height="600px" width="600px"></center>
+</form>
+<footer>
+    <center><h3>with <i class="fa fa-heart-o" style="font-size:30px;"></i>  Jacqu</h3></center>
+</footer>
+</body>
+</html>
